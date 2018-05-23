f84cdfcb856fb475cffaf750626424fc05b57cad
musiilhan <36209399+musiilhan@users.noreply.github.com>
2018-05-23 20:48:14 +0300

    Add files via upload

---
+++ Add,Delete,Update/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Musi\Documents\Database11.accdb");
+        int count = 0;
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText="insert into table1 values('"+textBox1.Text+"','"+textBox2.Text+"')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+
+            MessageBox.Show("Data inserted");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select*from table1";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from table1 where name='" + textBox3.Text + "'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Data deleted");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update table1 set name='" + textBox5.Text + "'where name='"+textBox4.Text+"'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Data updated");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select*from table1 where name='"+textBox1.Text+"'or city='"+textBox2.Text+"'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            da.Fill(dt);
+            count = Convert.ToInt32(dt.Rows.Count.ToString());
+            dataGridView1.DataSource = dt;
+
+            con.Close();
+            if (count == 0)
+            {
+                MessageBox.Show("record not found");
+            }
+        }
+    }
+}
